80bc90c9cd6f88d6314c66c1c9027c0852787419
Thomas Hornschuh <thomas@hornschuh-family.de>
2018-05-31 17:24:25 +0200

    Updated core to fusesoc release 1.8.2.dev53+gf4083d5

--- scripts/fusesoc_build.tcl
+++ scripts/fusesoc_build.tcl
@@ -1,3 +1,6 @@
+set_property board_part digilentinc.com:arty:part0:1.1 [current_project]
+set_property target_language VHDL [current_project]
+
 
 namespace eval _tcl {
 proc get_script_folder {} {
@@ -9,6 +12,10 @@ proc get_script_folder {} {
 
 variable script_folder
 set script_folder [_tcl::get_script_folder]
+
+# Add local ip repos
+set_property  ip_repo_paths  $script_folder/../ip_repo [current_project]
+update_ip_catalog
 
 
 # Import Block Design
